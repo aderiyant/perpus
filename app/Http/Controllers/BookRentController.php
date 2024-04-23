03348c181bbb0e941c897c06c52e859a1f9a559f
Dikry720p <dikrynurahman49@gmail.com>
2024-04-23 19:22:17 +0700

    update

--- app/Http/Controllers/BookRentController.php
+++ app/Http/Controllers/BookRentController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Controler\Auth;
 use App\Models\User;
 use App\Models\Buku;
 use App\Models\RentLogs;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class BookRentController extends Controller
 {
@@ -24,50 +24,50 @@ class BookRentController extends Controller
 
     public function store(Request $request)
     {
-         $request->validate([
-           'kodebuku' => 'required', // Anda bisa menambahkan aturan validasi sesuai kebutuhan
+        $request->validate([
+            'kodebuku' => 'required', // Anda bisa menambahkan aturan validasi sesuai kebutuhan
         ]);
 
-         // Menetapkan tanggal sewa dan tanggal pengembalian
-         $rent_date = Carbon::now()->toDateString();
-         $return_date = Carbon::now()->addDay(3)->toDateString();
+        // Menetapkan tanggal sewa dan tanggal pengembalian
+        $rent_date = Carbon::now()->toDateString();
+        $return_date = Carbon::now()->addDay(3)->toDateString();
 
-         $userId = Auth::id();
-         try {
-             DB::beginTransaction(); // Memulai transaksi database
+        $userId = Auth::id();
+        try {
+            DB::beginTransaction(); // Memulai transaksi database
 
-             // Memeriksa apakah buku tersedia
-             $buku = Buku::findOrFail($request->kodebuku);
-             if ($buku->status != 'tersedia') {
-                 throw new \Exception('Buku tidak tersedia untuk dipinjam');
-             }
+            // Memeriksa apakah buku tersedia
+            $buku = Buku::findOrFail($request->kodebuku);
+            if ($buku->status != 'tersedia') {
+                throw new \Exception('Buku tidak tersedia untuk dipinjam');
+            }
 
-    //         // Membuat entri peminjaman baru dalam RentLogs
-             $rentLogs = new RentLogs();
-             $rentLogs->kodebuku = $request->kodebuku;
-             $rentLogs->rent_date = $rent_date;
-             $rentLogs->return_date = $return_date;
-             $rentLogs->user_id = $userId;
-             $rentLogs->save();
-            
+            //         // Membuat entri peminjaman baru dalam RentLogs
+            $rentLogs = new RentLogs();
+            $rentLogs->kodebuku = $request->kodebuku;
+            $rentLogs->rent_date = $rent_date;
+            $rentLogs->return_date = $return_date;
+            $rentLogs->user_id = $userId;
+            $rentLogs->save();
 
-    //         // Mengubah status buku menjadi tidak tersedia (dipinjam)
-             $buku->status = 'tidak tersedia';
-          $buku->save();
-                //dd($buku);
-              DB::commit(); // Commit transaksi
 
-        //      // Redirect dengan pesan sukses
-              Session::flash('message', 'Buku berhasil dipinjam');
-              Session::flash('alert-class', 'alert-success');
-              return redirect('/buku');
-          } catch (\Throwable $th) {
-              DB::rollback(); // Rollback transaksi jika terjadi error
-              Session::flash('message', $th->getMessage());
-              Session::flash('alert-class', 'alert-danger');
-              return redirect('/buku');
-          }
-     }
+            //         // Mengubah status buku menjadi tidak tersedia (dipinjam)
+            $buku->status = 'tidak tersedia';
+            $buku->save();
+            //dd($buku);
+            DB::commit(); // Commit transaksi
+
+            //      // Redirect dengan pesan sukses
+            Session::flash('message', 'Buku berhasil dipinjam');
+            Session::flash('alert-class', 'alert-success');
+            return redirect('/buku');
+        } catch (\Throwable $th) {
+            DB::rollback(); // Rollback transaksi jika terjadi error
+            Session::flash('message', $th->getMessage());
+            Session::flash('alert-class', 'alert-danger');
+            return redirect('/buku');
+        }
+    }
 
 
     public function pengembalian()
@@ -79,22 +79,19 @@ class BookRentController extends Controller
     public function simpanpengembalian(Request $request)
     {
         $rent = RentLogs::where('user_id', $request->user_id)->where('kodebuku', $request->kodebuku)
-        ->where('actual_return_date', null);
+            ->where('actual_return_date', null);
         $rentData = $rent->first();
         $countData = $rent->count();
 
 
-        if($countData == 1) {
+        if ($countData == 1) {
             $rentData->actual_return_date = Carbon::now()->toDateString();
             $rentData->save();
 
             session::flash('message', 'Pengembalian Berhasil');
             session::flash('alert-class', 'alert-success');
             return redirect('pengembalian');
-              
-            
-        }
-        else {
+        } else {
             session::flash('message', 'tidak ada data pengembalian');
             session::flash('alert-class', 'alert-danger');
             return redirect('pengembalian');
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
 
     public function postlogin(Request $request)
     {
-        if (Auth::attempt($request->only('username', 'password')))
-        {
+        if (Auth::attempt($request->only('username', 'password'))) {
             return redirect('layouts');
         }
         return redirect('/');
