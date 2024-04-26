b6aeee53bac9fb75a28e6f77708ea0b9d4dad711
Dikry720p <dikrynurahman49@gmail.com>
2024-04-26 17:37:37 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,23 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
     public function postlogin(Request $request)
     {
-        $credentials = $request->validate([
-            'username' => 'required',
-            'password' => 'required'
-        ]);
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->route('buku')->with('success', 'You have successfully logged in!');
-        } else {
-            return back()->withErrors(['loginError' => 'Invalid credentials.']);
+        if (Auth::attempt($request->only('username', 'password'))) {
+            return redirect('layouts');
         }
+        return redirect('/');
+        //dd($request->all());
     }
 }
--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,29 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LOGIN</title>
     <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body style="position: relative" >
         <div style="width: 684px; height: 457px; left: 373px; top: 90px; position: relative; background: #D9D9D9"></div>
         <p class="paragraf">Selamat Datang</p>
         <div class="div1">
- <form action="{{ route('postlogin') }}" method="post">
+            <form action="{{ route('postlogin') }}" method="post">
                 {{ @csrf_field() }}
-  <div class="container">
-    <label for="username"><b>Username</b></label><br>
-    <input type="text" placeholder="Enter Username" name="username" required>
-    <br>
-    <label for="password"><b>Password</b></label><br>
-    <input type="password" placeholder="Enter Password" name="password" required>
-    <br>
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
+                <label for="username" style="left: 50px; top: 10px; position:  relative;">Username:</label><br>
+                <input type="text" id="username" name="username"
+                    style="width: 160px; height: 15px; left: 50px; top: 10px; position:  relative;" required><br>
 
-  <div class="container" style="background-color:#f1f1f1">
-  </div>
-</form>
+                <label for="password" style="left: 50px; top: 10px; position:  relative;">Password:</label><br>
+                <input type="password" id="password" name="password" color: #270E3F
+                    style="width: 160px; height: 15px; left: 50px; top: 10px; position:  relative;" required ><br>
+
+                <button type="submit" class="btnlogin btn-primary">LOGIN</button>
+            </form>
+        </div>
+
 </body>
 </html>
